e8da56b47df5281a81c69cf26cb3c348a830869d
Prasad Gorantla <yarlagaddap98@gmail.com>
2026-03-01 23:28:33 -0500

    Added a simple for loop for practice/test in main.cpp

---
+++ Desktop/radar-signal-sim-cpp/build/CMakeFiles/radar_sim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radar_sim.dir/src/main.cpp.obj"
+  "CMakeFiles/radar_sim.dir/src/main.cpp.obj.d"
+  "libradar_sim.dll.a"
+  "radar_sim.exe"
+  "radar_sim.exe.manifest"
+  "radar_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/radar_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Desktop/radar-signal-sim-cpp/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+    for (int n = 0; n <= 9; n++) {
+        std::cout << n << std::endl;
+    }
+    return 0;
+}
